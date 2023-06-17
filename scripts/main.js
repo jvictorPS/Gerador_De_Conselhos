76fc560e2a43de307dd269fbd0628259c43bac5e
João Victor <j.victorbahia@gmail.com>
2023-06-16 23:49:27 -0300

    função para requisitar o conselho e função para renderizar o conselho na tela. feito também a etilização parcial com criação das variaveis de cores e adição de algumas propriedades de css

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+async function pegarConselho() {
+    try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
+
+        const idConselho = data.slip.id
+        const textoConselho = data.slip.advice
+
+        return {idConselho , textoConselho}
+
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+}
+
+const botao = document.getElementById("buscar")
+const conselhoTexto = document.querySelector('#texto-conselho')
+const conselhoID = document.querySelector('#id-conselho')
+
+botao.addEventListener('click' , async () => {
+    try {
+        const resultado = await pegarConselho()
+    
+
+        conselhoID.innerText = `Adivice # ${resultado.idConselho}`
+        conselhoTexto.innerText = `"${resultado.textoConselho}"`
+
+      } catch (error) {
+        console.error('Erro:', error)
+      }
+})
